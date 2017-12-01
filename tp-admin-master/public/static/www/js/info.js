d1ac0a005317df9374a9eb7f899216343e1e565c
wangzhibin <wangzhibin@heyunchou.com>
2017-12-01 17:53:02 +0800

    123

--- tp-admin-master/public/static/www/js/info.js
+++ tp-admin-master/public/static/www/js/info.js
@@ -5,11 +5,7 @@ $(function(){
         var NickEmail=$('input[name=NickEmail]').val();
         var Gender=$('#sex option:selected').val();
         var BirthDay=$('input[name=BirthDay]').val(); 
-<<<<<<< HEAD
-        $.post('savepersonalinfo',{nickname:nickname,mobile:mobile,email:email,Gender:Gender,BirthDay:BirthDay},function(o){
-=======
         $.post('savepersonalinfo',{nickname:NickName,mobile:NickPhone,email:NickEmail,Gender:Gender,BirthDay:BirthDay},function(o){
->>>>>>> c7d5aeed6a4f3d683f969e8290894ff466aa2f43
             if(o.code == 1) {    
                 alert('修改成功！');                
             } else {
