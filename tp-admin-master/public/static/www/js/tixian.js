984bede43f944ad0f827a4410e70fe1abc672fc2
wangzhibin <wangzhibin@heyunchou.com>
2017-12-07 19:54:21 +0800

    20171207

--- tp-admin-master/public/static/www/js/tixian.js
+++ tp-admin-master/public/static/www/js/tixian.js
@@ -1,11 +1,16 @@
 $(function(){
-    
+    var yu = $.cookie("userInfo");
     $('.loginBtn').click(function(){        
-        var Money = $("#Money").val();     
+        var Money = $("#Money").val(); 
+         
         if(Money==''){
             alert('提现金额不能为空!');
             return false;
-        }        
+        }
+        if(Money=='0' || parseInt(Money)>parseInt(yu)){
+            alert('提现金额不足!');
+            return false;
+        }       
         $.post('dotixian',{Money:Money},function(o){
             if(o.code == 1) {    
                 alert('提交成功！');
